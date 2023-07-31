b93c970ae6697b864b8b762efa59b6b972ea60cd
gayathri <chamalkagayathri@gmail>
2023-07-31 19:48:51 +0530

    Complete task-1

---
+++ Demo1.java
@@ -0,0 +1,14 @@
+import java.math.BigDecimal;
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+
+        Double balance = 123456.7890;
+
+        System.out.printf("Balance = $%,.2f \n",balance);
+        
+    }
+
+    
+}

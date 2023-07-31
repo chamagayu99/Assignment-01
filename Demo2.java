53863d27fd830076184211704a2043f253e36310
gayathri <chamalkagayathri@gmail>
2023-07-31 19:50:04 +0530

    Complete task-2

---
+++ Demo2.java
@@ -0,0 +1,12 @@
+public class Demo2 {
+
+
+        public static void main(String[] args) {
+    
+            Double value = Math.PI;
+    
+            System.out.printf("Value of pi = %.3f \n",value);
+            
+        }
+    
+}
